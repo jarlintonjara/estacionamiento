943d040ad8cf763520fa5cfcba9b8320642d858f
fredy cumpa <fredy.acp25@gmail.com>
2022-05-31 16:17:35 -0500

    demo correo

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\ScheduleExportController;
 use App\Mail\SchedulesForTomorrow;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use \Illuminate\Support\Facades\Mail;
 use App\Http\Controllers\UserExportController;
+use App\Models\SettingModel;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +20,38 @@ use Illuminate\Support\Facades\DB;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get( 'test', function(){
+    $correo = SettingModel::first();
+
+    // $texto = "Hola";
+    // Storage::append("archivo.txt",$texto);
+    Storage::delete('schedule.xlsx');
+
+    $s = new ScheduleExportController;
+    $s->export();
+
+    $email = new SchedulesForTomorrow;
+
+    $correos = [];
+    if ($correo->email1 != NULL or $correo->email1 != '') {
+        array_push($correos, $correo->email1);
+    }
+    if ($correo->email2 != NULL or $correo->email2 != '') {
+        array_push($correos, $correo->email2);
+    }
+    if ($correo->email3 != NULL or $correo->email3 != '') {
+        array_push($correos, $correo->email3);
+    }
+    if ($correo->email4 != NULL or $correo->email4 != '') {
+        array_push($correos, $correo->email4);
+    }
+
+    Mail::to($correo->email)
+        ->cc($correos)
+        ->send($email);
+    return "Mensaje enviado";
+});
 
 Route::get('/{any}', function () {
     return view('welcome');
