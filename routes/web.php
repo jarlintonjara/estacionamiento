54ef9c924cf0feb08c4b10cb91423cb6c13c1477
fredy cumpa <fredy.acp25@gmail.com>
2022-09-28 11:58:37 -0500

    test email

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,15 @@
 <?php
 
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\ScheduleExportController;
+use App\Mail\SchedulesForTomorrow;
+use App\Models\ProgramacionModel;
+use App\Models\SettingModel;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,7 +23,35 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('test', function(){
-    
+    $correo = SettingModel::first();
+
+        // $texto = "Hola";
+        // Storage::append("archivo.txt",$texto);
+        Storage::delete('schedule.xlsx');
+
+        $s = new ScheduleExportController;
+        $s->export();
+
+        $email = new SchedulesForTomorrow;
+
+        $correos = [];
+        if ($correo->email1 != NULL OR $correo->email1 != '') {
+            array_push($correos,$correo->email1);
+        } 
+        if ($correo->email2 != NULL OR $correo->email2 != '') {
+            array_push($correos,$correo->email2);
+        } 
+        if ($correo->email3 != NULL OR $correo->email3 != '') {
+            array_push($correos,$correo->email3);
+        } 
+        if ($correo->email4 != NULL OR $correo->email4 != '') {
+            array_push($correos,$correo->email4);
+        }    
+        
+        Mail::to($correo->email)
+        ->cc($correos)
+        ->send($email);
+        return "Mensaje enviado";
 });
 
 Route::get('event/{user}/{estacionamiento}', [EventController::class, 'programming'])
