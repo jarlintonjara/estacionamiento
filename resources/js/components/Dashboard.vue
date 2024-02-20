c7a110c17bce62ff5fdfb2513f0e45ff2d93fd3c
Estiven Mayhuay <developerwebhairton@gmail.com>
2024-02-19 19:08:43 -0500

    agregando cambios

--- resources/js/components/Dashboard.vue
+++ resources/js/components/Dashboard.vue
@@ -3,7 +3,7 @@
         <div class="content">
             <div class="subheader">
                 <h1 class="subheader-title">
-                    <i class='subheader-icon fal fa-chart-area'></i> <span class='fw-300'>Dashboard -  testing</span>
+                    <i class='subheader-icon fal fa-chart-area'></i> <span class='fw-300'>Dashboard</span>
                 </h1>
             </div>
             
